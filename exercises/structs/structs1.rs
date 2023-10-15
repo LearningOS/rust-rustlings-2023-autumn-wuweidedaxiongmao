7a3c61578d84e6609e19fd1a5ee69986000ae896
wuweidedaxiongmao <122270905+wuweidedaxiongmao@users.noreply.github.com>
2023-10-15 08:42:42 +0000

    update in 10_15

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -5,7 +5,7 @@
 // Execute `rustlings hint structs1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 struct ColorClassicStruct {
     // TODO: Something goes here
@@ -27,9 +27,9 @@ mod tests {
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
         let green =ColorClassicStruct{
-            red:1,
-            green:2,
-            blue:3,
+            red:0,
+            green:255,
+            blue:0,
         };
 
         assert_eq!(green.red, 0);
@@ -40,7 +40,7 @@ mod tests {
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
-        let green =ColorTupleStruct(1,2,2);
+        let green =ColorTupleStruct(0,255,0);
 
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
